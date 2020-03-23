12e4e69682a4eba937fcba8d19ee99b403afe1c7
Zongyuan Chen <zongyc@cs.washington.edu>
2020-03-22 22:16:36 -0700

    finish

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverse) {m <<- inverse}
+  getInverse <- function() {m}
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
-
+# If the x had been solved before, then it would be stored in cache.
+# In this case, we directly retrieve the result from the cache instead
+# of solving it again
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("Computed and cached before.")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setInverse(m)
+  m
 }
